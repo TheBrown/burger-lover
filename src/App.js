6e6a036b9095b119eb75f88e6a571fa20f9e7609
Saleumsack <saleumsack16@gmail.com>
2018-10-02 00:51:26 +0700

    Add Layout Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import Layout from "./components/Layout/Layout";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Layout>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore
+            sint recusandae optio, voluptates velit similique inventore totam
+            cupiditate animi qui eos dolorem maiores doloribus distinctio
+            doloremque eveniet facilis assumenda quaerat.
+          </p>
+        </Layout>
       </div>
     );
   }

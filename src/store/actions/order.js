d7ec43e55bc76739993b8d177f9da7f00ef1935e
Saleumsack <saleumsack16@gmail.com>
2018-10-14 17:08:52 +0700

    Fetching Order Using Redux

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -39,5 +39,43 @@ export const purchaseBurger = orderData => {
 export const purchaseInit = () => {
   return {
     type: actionType.PURCHASE_INIT
+  };
+};
+
+export const fetchOrdersSuccess = orders => {
+  return {
+    type: actionType.FETCH_ORDERS_SUCCESS,
+    orders: orders
+  };
+};
+
+export const fetchOrdersFail = error => {
+  return {
+    type: actionType.FETCH_ORDERS_FAIL,
+    error: error
+  };
+};
+
+export const fetchOrdersStart = () => {
+  return {
+    type: actionType.FETCH_ORDERS_START
+  };
+};
+
+export const fetchOrders = () => {
+  return dispatch => {
+    dispatch(fetchOrdersStart());
+    axios
+      .get("/orders.json")
+      .then(res => {
+        const fetchedOrder = [];
+        for (let key in res.data) {
+          fetchedOrder.push({ ...res.data[key], id: key });
+        }
+        dispatch(fetchOrdersSuccess(fetchedOrder));
+      })
+      .catch(err => {
+        dispatch(fetchOrdersFail(err));
+      });
   }
 }
